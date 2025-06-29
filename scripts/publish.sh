6370c72b4e458689e96dd32b80912c45a948cd0a
nicklin99 <authen@qq.com>
2025-06-29 22:00:12 +0800

    init

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 #  pnpm login --registry https://registry.npmjs.org
-pnpm publish --filter ./packages/* --access public --registry https://registry.npmjs.org
+pnpm publish --filter="./packages/*" --access public --registry https://registry.npmjs.org
